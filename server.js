4df19fc1f015120556e15502c64a3d67ff52267f
restbackDev <jason.rbdev@gmail.com>
2025-03-10 11:00:15 -0700

    wip: adds root route

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
-const app = express();
-
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
 
-// Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
+// initialize express app
+const app = express();
 
+//config settings
+dotenv.config();
+const port = process.env.PORT || "3000";
+//const port = process.env.PORT ? process.env.PORT : "3000"; //using ternary operation
+
+// connect to mongoDB
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// mount middleware:
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
@@ -23,6 +26,12 @@ app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
 
+// mount routes
+app.get('/', (req, res) => {
+  res.send('index.ejs');
+});
+
+// tells the app the listen for HTTP requests 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
