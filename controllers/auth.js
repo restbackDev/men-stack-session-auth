d07804bf97e1f60eb45f795e0842248e703520bd
restbackDev <jason.rbdev@gmail.com>
2025-03-17 09:59:20 -0700

    wip: cointrollers

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,77 @@
 const express = require("express");
 const router = express.Router(); //route objects, only exclusively only for routing purposes
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
-//NOTE: there are no routes
+
+//NOTE: there are no routes here yet
 
 router.get("/sign-up", (req, res) => {
   res.render("auth/sign-up.ejs");
 });
 // the routwer object is similar to the app object in server js
 // however. it only has router functionality
+
+router.post("/sign-up", async (req,res) => {
+  // check if the user is similar to the app - NO DUPLICATE USERNAMES
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+    return res.send("Username already taken.");
+  }
+  //check if the password and confirm password are a match?
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+  }
+
+  // create encrypterd version of plain-text password(hashed and salted)
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  const user = await User.create(req.body);
+  res.send(`Thanks for signing up ${user.username}`);
+});
+
+// GET /sign-in: send a page that has a login form
+router.get("/sign-in", (req, res) => {
+  res.render("auth/sign-in.ejs");
+});
+
+
+// POST signin - route that will be used when login form
+// is submitted
+router.post("/sign-in", async (req, res) => {
+  const userInDatabase = await User.findOne({
+    username: req.body.username
+  });
+
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  // bcrypy's comparison function
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+  
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+  // at this point, we've made it past verification
+  // There is a user AND they had the correct password. Time to make a session!
+  // Avoid storing the password, even in hashed format, in the session
+  // If there is other data you want to save to `req.session.user`, do so here!
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id
+  };
+  console.log(req.session);
+  res.redirect('/');
+});
+
+router.get('/signout', (req,res) => {
+  req.session.destroy(); //destroying the session obj "ends" the session
+  res.redirect("/"); // return home
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const authController = require("/.controllers/auth");
+const authController = require("./controllers/auth");
+const session =require("express-session");
+
 
 // initialize express app
 const app = express();
 
 //config settings
 dotenv.config();
-const port = process.env.PORT || "3000";
-//const port = process.env.PORT ? process.env.PORT : "3000"; //using ternary operation
+const port = process.env.PORT || "3001";
+// or use this version below
+// const port = process.env.PORT ? process.env.PORT : "3000"; //using ternary operation
 
 // connect to mongoDB
 mongoose.connect(process.env.MONGODB_URI);
@@ -20,12 +23,14 @@ mongoose.connection.on("connected", () => {
 });
 
 // mount middleware:
-// Middleware to parse URL-encoded data from forms
-app.use(express.urlencoded({ extended: false }));
-// Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride("_method"));
-// Morgan for logging HTTP requests
-app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded data from forms
+app.use(methodOverride("_method")); // Middleware for using HTTP verbs such as PUT or DELETE
+app.use(morgan('dev')); // Morgan for logging HTTP requests
+app.use(session ({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use("/auth", authController);
 // any HTTP request from browser that comes to/ auth...
@@ -34,10 +39,22 @@ app.use("/auth", authController);
 
 // mount routes
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {
+    user: req.session.user
+  }) 
 });
+
+//protected routes - user listewnbr logged in for access
+app.get('/vip-lounge', (req,res) => {
+  if (req.session.user) {
+    res.send("Welcome to the VIP L");
+  }else {
+    res.send("Sorry,  yo must be logged in for that");
+  }
+})
 
 // tells the app the listen for HTTP requests 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
+
